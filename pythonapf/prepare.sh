d4ca5f054b9b9db0f035adc1dbb9eba77b46845b
Stavros Charismiadis <stauros411@gmail.com>
2022-06-27 12:35:58 +0300

    Change Common Name of CAPIF in properties file

--- pythonapf/prepare.sh
+++ pythonapf/prepare.sh
@@ -1,5 +1,5 @@
 curl --request GET 'http://host.docker.internal:8080/ca-root' 2>/dev/null | jq -r '.certificate' -j > ca.crt
 
-echo '172.17.0.1      capifcore' >> /etc/hosts
+echo '172.17.0.1      openshift.evolved-5g.eu' >> /etc/hosts
 
 tail -f /dev/null
