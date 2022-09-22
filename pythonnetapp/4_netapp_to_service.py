0c1591a9f148fcfa578985a587b663bb14b85f7e
nacho <hjoseigancio@gmail.com>
2022-09-22 11:25:45 +0200

    https to http

--- pythonnetapp/4_netapp_to_service.py
+++ pythonnetapp/4_netapp_to_service.py
@@ -13,7 +13,7 @@ REDIS_PORT = os.environ.get('REDIS_PORT')
 def demo_to_aef(demo_ip, demo_port, demo_url, jwt_token):
 
     print("Using AEF Service API")
-    url = "https://{}:{}{}".format(demo_ip, demo_port, demo_url)
+    url = "http://{}:{}{}".format(demo_ip, demo_port, demo_url)
     #url = "http://python_aef:8086/hello"
 
     payload = json.dumps({
