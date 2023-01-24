e15414d085daf40e77d591112a7e5a22c78bd568
Stavros Charismiadis <stauros411@gmail.com>
2023-01-24 13:32:44 +0200

    Alignment with version 3.0 of CAPIF

--- pythonnetapp/prepare.sh
+++ pythonnetapp/prepare.sh
@@ -1,3 +1,9 @@
-evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
+#evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
+
+curl  --connect-timeout 5 \
+    --max-time 10 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    --request GET "http://$CAPIF_HOSTNAME:$CAPIF_PORT/ca-root" 2>/dev/null | jq -r '.certificate' -j > ca.crt
 
 tail -f /dev/null
