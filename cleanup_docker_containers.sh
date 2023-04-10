f5d4609719f9c4c2e6bc47b7d6f8ed3bbd08e79f
Stavros Charismiadis <stauros411@gmail.com>
2023-04-10 15:00:06 +0300

    Move container folders to src

--- cleanup_docker_containers.sh
+++ cleanup_docker_containers.sh
@@ -3,13 +3,13 @@
 docker-compose down --rmi all --remove-orphans || true
 
 (
-sudo rm ./pythonnetapp/capif_onboarding/*
-sudo rm ./pythonnetapp/demo_values.json
-sudo rm ./pythonnetapp/ca.crt
-sudo rm ./pythonnetapp/cert_req.csr
-sudo rm ./pythonnetapp/dummy.crt
-sudo rm ./pythonnetapp/private.key
-sudo rm ./pythonnetapp/ca_service.crt
+sudo rm ./src/pythonnetapp/capif_onboarding/*
+sudo rm ./src/pythonnetapp/demo_values.json
+sudo rm ./src/pythonnetapp/ca.crt
+sudo rm ./src/pythonnetapp/cert_req.csr
+sudo rm ./src/pythonnetapp/dummy.crt
+sudo rm ./src/pythonnetapp/private.key
+sudo rm ./src/pythonnetapp/ca_service.crt
 )
 
 # sudo rm ./pythonnetapp/demo_values.json && sudo rm ./pythonnetapp/ca.crt && sudo rm ./pythonnetapp/cert_req.csr && sudo rm ./pythonnetapp/dummy.crt && sudo rm ./pythonnetapp/private.key && sudo rm ./pythonnetapp/ca_service.crt
