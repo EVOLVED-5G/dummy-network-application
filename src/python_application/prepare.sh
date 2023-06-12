eee6c83ba4886c0345a8320fb30d6ed23494429b
Stavros Charismiadis <stauros411@gmail.com>
2023-06-12 13:34:58 +0300

    testing new sdk version (taking from sdk develop branch)

--- src/python_application/prepare.sh
+++ src/python_application/prepare.sh
@@ -6,6 +6,6 @@ curl  --connect-timeout 5 \
     --retry-max-time 40 \
     --request GET "http://$CAPIF_HOSTNAME:$CAPIF_PORT/ca-root" 2>/dev/null | jq -r '.certificate' -j > ca.crt
 
-evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json"
+evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json" --environment="development"
 
 tail -f /dev/null
